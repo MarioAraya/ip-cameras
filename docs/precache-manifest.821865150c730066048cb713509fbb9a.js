c5de76795cddc2ecd3ed574fc91f6ba9f6441a3c
Mario Araya Romero <arayaromero@gmail.com>
2019-05-22 01:02:34 -0400

    docs folder with build content

---
+++ docs/precache-manifest.821865150c730066048cb713509fbb9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7760aff412e0b53f81b27ebf145715d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feff0370b20b1df9e374",
+    "url": "/static/css/main.a8ef3f19.chunk.css"
+  },
+  {
+    "revision": "ec7ca03bb7d99593546c",
+    "url": "/static/js/2.c9ee9d95.chunk.js"
+  },
+  {
+    "revision": "feff0370b20b1df9e374",
+    "url": "/static/js/main.2b6f2ffd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
